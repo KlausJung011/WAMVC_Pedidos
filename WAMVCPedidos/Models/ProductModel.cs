6394f864372f4d0080a67b017f7c89b4852a4fbf
KlausJung011 <nicolas.nsavs@gmail.com>
2025-09-16 20:42:01 -0400

    Creados y validados UserModel y ProductModel

---
+++ WAMVCPedidos/Models/ProductModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WAMVCPedidos.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nombre del producto")]
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 100 caracteres")]
+        //[Remote(action: "CheckNombreUnico", controller: "Productos", AdditionalFields = nameof(Id), ErrorMessage = "Ya existe un producto con ese nombre.")]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Descripción")]
+        [Required(ErrorMessage = "La descripción es obligatoria")]
+        [StringLength(500)]
+        public string Descripcion { get; set; }
+
+        [Display(Name = "Precio")]
+        [Required(ErrorMessage = "El precio es obligatorio")]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(0.01, 99999999.99, ErrorMessage = "El precio debe estar entre 0.01 y 99,999,999.99")]
+        [RegularExpression(@"^\d{1,8}(\.\d{1,2})?$", ErrorMessage = "Máximo 8 dígitos antes y 2 después del punto decimal")]
+        public decimal Precio { get; set; }
+
+        [Display(Name = "Cantidad en stock")]
+        [Required(ErrorMessage = "El stock es obligatorio")]
+        [Range(0, 9999999, ErrorMessage = "El stock no puede ser negativo")]
+        public int Stock { get; set; }
+
+        [Display(Name = "Categroía del producto")]
+        [Required(ErrorMessage = "La categoría es obligatoria")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "La categoría debe tener entre 3 y 100 caracteres")]
+        //[Remote(action: "CheckNombreUnico", controller: "Productos", AdditionalFields = nameof(Id), ErrorMessage = "Ya existe un producto con ese nombre.")]
+        public string Categoria { get; set; }
+
+        //Un producto puede estar en muchos detalles de pedido 1-N
+        public ICollection<OrderItemModel>? OrderItems { get; set; }
+    }
+}
---
+++ WAMVCPedidos/Models/UserModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WAMVCPedidos.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nombre del usuario")]
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 100 caracteres")]
+        //[Remote(action: "CheckNombreUnico", controller: "Clientes", AdditionalFields = nameof(Id), ErrorMessage = "Ya existe un cliente con ese nombre.")]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Correo electrónico")]
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [StringLength(100, MinimumLength = 7, ErrorMessage = "El email debe tener entre 7 y 100 caracteres")]
+        [EmailAddress(ErrorMessage = "Formato de correo electrónico inválido.")]
+        [RegularExpression(@"^[\w\.-]+@[\w\.-]+\.\w{2,4}$", ErrorMessage = "El correo no cumple el formato requerido.")]
+        public string Email { get; set; }
+
+        [Display(Name = "Contraseña del usuario")]
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "La contraseña debe tener entre 4 y 20 caracteres")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "El rol es obligatorio")]
+        [Display(Name = "Rol")]
+        [StringLength(9, ErrorMessage = "El rol no puede exceder los 9 caracteres")]
+        [RegularExpression(@"^(admin|cliente|empleado)$", ErrorMessage = "El rol debe ser admin, cliente o empleado")]
+        public string Rol { get; set; }
+
+        //Un cliente puede tener varias órdenes
+        public ICollection<OrderModel>? Orders { get; set; }
+    }
+}
