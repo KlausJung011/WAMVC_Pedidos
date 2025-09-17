2e5995f13d72bdb77dabc5b6b7b1e86cd9fc8673
KlausJung011 <nicolas.nsavs@gmail.com>
2025-09-16 21:09:21 -0400

    OrderItemModel, Product FK y Validaciones DataAnnotations

--- WAMVCPedidos/Models/OrderItemModel.cs
+++ WAMVCPedidos/Models/OrderItemModel.cs
@@ -5,6 +5,31 @@ namespace WAMVCPedidos.Models
 {
     public class OrderItemModel
     {
-        
+        public int Id { get; set; }
+
+        [Display(Name = "Fecha de la orden")]
+        [Required(ErrorMessage = "El ID de la orden es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "El ID de pedido debe ser mayor a 0")]
+        public int IdOrder { get; set; }
+
+        [Display(Name = "Nombre del Producto")]
+        [Required(ErrorMessage = "El nombre del producto es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "El ID de pedido debe ser mayor a 0")]
+        public int IdProduct { get; set; }
+
+        [Display(Name = "Cantidad")]
+        [Required(ErrorMessage = "La cantidad es obligatorio")]
+        [Range(1, 9999999, ErrorMessage = "La cantidad tiene que ser mayor a 0")]
+        public int Cantidad { get; set; }
+
+        [Display(Name = "Subtotal del producto")]
+        [Required(ErrorMessage = "El subtotal es obligatorio")]
+        [Column(TypeName = "decimal(10,2)")]
+        [Range(0.01, 99999999.99, ErrorMessage = "El subtotal debe estar entre 0.01 y 99,999,999.99")]
+        [RegularExpression(@"^\d{1,8}(\.\d{1,2})?$", ErrorMessage = "Máximo 8 dígitos antes y 2 después del punto decimal")]
+        public decimal Subtotal { get; set; }
+
+        public OrderModel? Order { get; set; }
+        public ProductModel? Product { get; set; }
     }
 }
